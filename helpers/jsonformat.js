c8535799039075af53dc4cc1f40df624989f6925
Laxman Pokhrel <Laxman.Pokhrel@studentambassadors.com>
2021-12-17 21:25:30 +0545

    tech news scrapped from ekantipur

---
+++ helpers/jsonformat.js
@@ -0,0 +1,35 @@
+let data = {
+    "news":
+    {
+        "ekantipur": {
+            "frontPageHeadlines": {
+                "latestCount": 0,
+                "items": []
+            },
+            "sports": {
+                "latestCount": 0,
+                "items": []
+            },
+            "technology": {
+                "latestCount": 0,
+                "items": []
+            }
+        },
+        "setopati": {
+            "frontPageHeadlines": {
+                "latestCount": 0,
+                "items": []
+            },
+            "sports": {
+                "latestCount": 0,
+                "items": []
+            },
+            "technology": {
+                "latestCount": 0,
+                "items": []
+            }
+        }
+    }
+    ,
+}
+module.exports = data;
--- scrapKantipur.js
+++ scrapKantipur.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const JSONDFile = require('./data.json');
+const dataPrototype = require("./helpers/jsonformat");
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   //* to scrap front page headlines
@@ -27,49 +28,66 @@ const JSONDFile = require('./data.json');
       });
     }
     return data;
+  }).then(() => {
+    console.log(" front page headlines fetched!!");
+  }).catch(err => {
+    console.log("Error While Inserting frontPAgeHeadlines\n:" + err);
   });
-
+  //add to data
+  dataPrototype.news.ekantipur.frontPageHeadlines.items.push(frontPageHeadlines);
+  console.log()
   //* to scrap sports news
   await page.goto("https://ekantipur.com/sports");
   const sportsHeadlines = await page.evaluate(() => {
     const sportsData = [];
     const catagory = document.querySelector(".listLayout .row > .cat_name > .catName").textContent;
     const sportsArticles = document.querySelectorAll(".listLayout .row > div:nth-child(2) article");
-    //we get objects from above selector. To loop through each object we have to destructure as to an array... 
+    //* we get objects from above selector. To loop through each object we have to destructure as to an array... 
     temp = [...sportsArticles];
-    // for (let i = 0; i < temp.length; i++) {
-    //   sportsData.push({
-    //     headline: temp[i].childNodes[0].childNodes[0].childNodes[0] ? temp[i].childNodes[0].childNodes[0].childNodes[0].textContent : "missing",
-    //     headlineNavigation: temp[i].childNodes[0].childNodes[0].childNodes[0] ? temp[i].childNodes[0].childNodes[0].childNodes[0].getAttribute("href") : "missing",
-    //     author: temp[i].childNodes[0].childNodes[1].childNodes[0] ? temp[i].childNodes[0].childNodes[1].childNodes[0].textContent : "missing",
-    //     authorProfileNavigation: temp[i].childNodes[0].childNodes[1].childNodes[0] ? temp[i].childNodes[0].childNodes[1].childNodes[0].getAttribute("href") : "missing",
-    //     hedlineSummery: temp[i].childNodes[0].childNodes[2] ? temp[i].childNodes[0].childNodes[2].textContent : "missing",
-    //     mediaSrc: temp[i].childNodes[1].childNodes[0].childNodes[0].childNodes[0] ? temp[i].childNodes[1].childNodes[0].childNodes[0].childNodes[0].getAttribute("data-src") : "missing",
-    //     mediaNavigation: temp[i].childNodes[1].childNodes[0].childNodes[0] ? temp[i].childNodes[1].childNodes[0].childNodes[0].getAttribute("href") : "missing"
-    //   });
-    // }
     temp.forEach(x => {
       sportsData.push({
-        headline: x.childNodes[0].childNodes[0].childNodes[0] ? x.childNodes[0].childNodes[0].childNodes[0].textContent : "missing",
-        headlineNavigation: x.childNodes[0].childNodes[0].childNodes[0] ? x.childNodes[0].childNodes[0].childNodes[0].getAttribute("href") : "missing",
-        author: x.childNodes[0].childNodes[1].childNodes[0] ? x.childNodes[0].childNodes[1].childNodes[0].textContent : "missing",
-        authorProfileNavigation: x.childNodes[0].childNodes[1].childNodes[0] ? x.childNodes[0].childNodes[1].childNodes[0].getAttribute("href") : "missing",
-        hedlineSummery: x.childNodes[0].childNodes[2] ? x.childNodes[0].childNodes[2].textContent : "missing",
-        mediaSrc: x.childNodes[1].childNodes[0].childNodes[0].childNodes[0] ? x.childNodes[1].childNodes[0].childNodes[0].childNodes[0].getAttribute("data-src") : "missing",
-        mediaNavigation: x.childNodes[1].childNodes[0].childNodes[0] ? x.childNodes[1].childNodes[0].childNodes[0].getAttribute("href") : "missing"
+        "headline": x.childNodes[0].childNodes[0].childNodes[0] ? x.childNodes[0].childNodes[0].childNodes[0].textContent : "missing",
+        "headlineNavigation": x.childNodes[0].childNodes[0].childNodes[0] ? x.childNodes[0].childNodes[0].childNodes[0].getAttribute("href") : "missing",
+        "author": x.childNodes[0].childNodes[1].childNodes[0] ? x.childNodes[0].childNodes[1].childNodes[0].textContent : "missing",
+        "authorProfileNavigation": x.childNodes[0].childNodes[1].childNodes[0] ? x.childNodes[0].childNodes[1].childNodes[0].getAttribute("href") : "missing",
+        "hedlineSummery": x.childNodes[0].childNodes[2] ? x.childNodes[0].childNodes[2].textContent : "missing",
+        "mediaSrc": x.childNodes[1].childNodes[0].childNodes[0].childNodes[0] ? x.childNodes[1].childNodes[0].childNodes[0].childNodes[0].getAttribute("data-src") || x.childNodes[1].childNodes[0].childNodes[0].childNodes[0].getAttribute("src") : "missing",
+        "mediaNavigation": x.childNodes[1].childNodes[0].childNodes[0] ? x.childNodes[1].childNodes[0].childNodes[0].getAttribute("href") : "missing"
       });
     })
     return sportsData;
+  }).then(() => {
+    console.log(" sports headlines fetched!!");
+  }).catch(err => {
+    console.log("Error While fetching sports news\n:" + err);
   });
-  // const dataRead = JSON.parse(fs.readFileSync("data.json"));
-  // console.log(dataRead);
-  // fs.writeFile("data.json", JSON.stringify(frontPageHeadlines), (error) => {
-  //   console.log("error occured");
-  // });
-  // JSONData.news.ekantipur.frontPageHeadlines.push();
-  // JSONData.news.push(frontPageHeadlines);
-  console.log(frontPageHeadlines);
-  console.log(sportsHeadlines);
-  // console.log("Result is :", JSONData.news.ekantipur.frontPageHeadlines.push(frontPageHeadlines));
+
+  //* to screp technology news
+  await page.goto("https://ekantipur.com/technology");
+  const technologyNews = await page.evaluate(() => {
+    const data = [];
+    const catagory = document.querySelector(".listLayout .row > .cat_name > .catName").textContent;
+    const techArticles = document.querySelectorAll(".listLayout .row > div:nth-child(2) article");
+    temp = [...techArticles];
+    temp.forEach((x, index) => {
+      data.push({
+        "headline": x.childNodes[0].childNodes[0].childNodes[0] ? x.childNodes[0].childNodes[0].childNodes[0].textContent : "missing",
+        "headlineNavigation": x.childNodes[0].childNodes[0].childNodes[0] ? x.childNodes[0].childNodes[0].childNodes[0].getAttribute("href") : "missing",
+        "author": x.childNodes[0].childNodes[1].childNodes[0] ? x.childNodes[0].childNodes[1].childNodes[0].textContent : "missing",
+        "authorProfileNavigation": x.childNodes[0].childNodes[1].childNodes[0] ? x.childNodes[0].childNodes[1].childNodes[0].getAttribute("href") : "missing",
+        "hedlineSummery": x.childNodes[0].childNodes[2] ? x.childNodes[0].childNodes[2].textContent : "missing",
+        "mediaSrc": x.childNodes[1].childNodes[0].childNodes[0].childNodes[0] ? x.childNodes[1].childNodes[0].childNodes[0].childNodes[0].getAttribute("data-src") || x.childNodes[1].childNodes[0].childNodes[0].childNodes[0].getAttribute("src") : "missing",
+        "mediaNavigation": x.childNodes[1].childNodes[0].childNodes[0] ? x.childNodes[1].childNodes[0].childNodes[0].getAttribute("href") : "missing"
+      });
+    })
+    console.log(data);
+
+    return data;
+  }).then(() => {
+    console.log("tech news fetched!!");
+
+  }).catch((error) => {
+    console.log("error while fetching tech news : \n" + error);
+  });
   await browser.close();
 })();
