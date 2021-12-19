7cb8130e21706d14c421b0bb7777a7b53d224912
Laxman Pokhrel <Laxman.Pokhrel@studentambassadors.com>
2021-12-19 21:13:56 +0545

    started working on backend

---
+++ scrapHamroPatro.js
@@ -0,0 +1,31 @@
+const puppiter = require('puppeteer')
+const fs = require('fs/promises')
+
+async function scrapHamroPatro() {
+    const scrapFromHamroPatro = 'https://www.hamropatro.com/'
+    const browser = await puppiter.launch({ headless: true })
+    const page = await browser.newPage()
+    await page.goto(scrapFromHamroPatro)
+
+    const result = await page.evaluate(() => {
+        const nepDate = document.querySelector('.logo .date .nep').textContent
+        console.log('Nepali event: ' + nepEvent)
+        const scrappedNepalTime =
+            document.querySelector('.logo .time span').textContent || 'not available'
+        const engDate =
+            document.querySelector('.logo .time .eng').textContent || 'not available'
+        return {
+            source: 'hamropatro',
+            data: {
+                nepDate: nepDate,
+                nepEvent: nepEvent,
+                scrappedNepalTime: scrappedNepalTime,
+                engDate: engDate,
+            },
+        }
+    })
+    console.log(result)
+    await browser.close()
+}
+
+scrapHamroPatro()
